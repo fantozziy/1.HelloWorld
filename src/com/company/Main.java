406502eab13b80e81b9ec1f9ba32ed2112b3f37a
pst <ruslan.ermeichook@gmail.com>
2017-03-15 18:54:14 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!"); // вывод на консоль Hello World!
+        System.out.println("That`s Sergey."); // вывод на консоль That`s Sergey.
+
+    }
+}
